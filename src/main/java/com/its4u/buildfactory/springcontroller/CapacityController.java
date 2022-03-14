2797ffaa044ee4cca5634d33d0502e2321302015
'ksc' <'ksc@example.com';>
2022-03-14 18:58:32 +0100

    rderyr

--- src/main/java/com/its4u/buildfactory/springcontroller/CapacityController.java
+++ src/main/java/com/its4u/buildfactory/springcontroller/CapacityController.java
@@ -1,4 +1,6 @@
 import org.springframework.stereotype.Controller;
+import javax.servlet.http.HttpServletRequest;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class CapacityController {
