4671c6be099d32c218b275451f0f07b0995c63e3
'ksc' <'ksc@example.com';>
2022-03-14 19:06:32 +0100

    dfsfs

--- src/main/java/com/its4u/buildfactory/springcontroller/CapacityController.java
+++ src/main/java/com/its4u/buildfactory/springcontroller/CapacityController.java
@@ -1,11 +1,12 @@
+package com.its4u.buildfactory.springcontroller;
+
 import org.springframework.stereotype.Controller;
-import javax.servlet.http.HttpServletRequest;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class CapacityController {
 	
-	private final HttpServletRequest request;
+
 	
 	@GetMapping(value = "/")
 	public String getHome() {
