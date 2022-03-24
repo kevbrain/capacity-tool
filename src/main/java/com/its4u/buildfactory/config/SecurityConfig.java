49da2cdcafbc023004a2dd79e0b29ec648642249
'ksc' <'ksc@example.com';>
2022-03-24 17:18:02 +0100

    keycloak

--- src/main/java/com/its4u/buildfactory/config/SecurityConfig.java
+++ src/main/java/com/its4u/buildfactory/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.its4u.buildfactory.config;
 
+import org.keycloak.adapters.springsecurity.KeycloakConfiguration;
 import org.keycloak.adapters.springsecurity.authentication.KeycloakAuthenticationProvider;
 import org.keycloak.adapters.springsecurity.config.KeycloakWebSecurityConfigurerAdapter;
 import org.keycloak.adapters.springsecurity.management.HttpSessionManager;
@@ -13,47 +14,35 @@ import org.springframework.security.core.session.SessionRegistryImpl;
 import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy;
 import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
 
+@KeycloakConfiguration
 public class SecurityConfig extends KeycloakWebSecurityConfigurerAdapter {
 	
 	@Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth) {
-        SimpleAuthorityMapper grantedAuthorityMapper = new SimpleAuthorityMapper();
-        grantedAuthorityMapper.setPrefix("ROLE_");
-
-        KeycloakAuthenticationProvider keycloakAuthenticationProvider = keycloakAuthenticationProvider();
-        keycloakAuthenticationProvider.setGrantedAuthoritiesMapper(grantedAuthorityMapper);
+    public void configureGlobal(
+      AuthenticationManagerBuilder auth) throws Exception {
+ 
+        KeycloakAuthenticationProvider keycloakAuthenticationProvider
+          = keycloakAuthenticationProvider();
+        keycloakAuthenticationProvider.setGrantedAuthoritiesMapper(
+          new SimpleAuthorityMapper());
         auth.authenticationProvider(keycloakAuthenticationProvider);
     }
 
-
-	@Bean
-	@Override
-	protected SessionAuthenticationStrategy sessionAuthenticationStrategy() {
-		// TODO Auto-generated method stub
-		return new RegisterSessionAuthenticationStrategy(new SessionRegistryImpl());
-	}
-
-	@Bean
+    @Bean
     @Override
-    @ConditionalOnMissingBean(HttpSessionManager.class)
-    protected HttpSessionManager httpSessionManager() {
-        return new HttpSessionManager();
+    protected SessionAuthenticationStrategy sessionAuthenticationStrategy() {
+        return new RegisterSessionAuthenticationStrategy(
+          new SessionRegistryImpl());
     }
-	
-	/**
-     * Define security constraints for the application resources.
-     */
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         super.configure(http);
-        
-        /*
-        http
-            .authorizeRequests()
-            //.antMatchers("/dashboard").hasAnyRole("Member", "Librarian")
-            //.antMatchers("/manager").hasRole("Librarian")
-            .anyRequest().permitAll();
-        */
+        http.authorizeRequests()
+          .antMatchers("/customers*")
+          .hasRole("user")
+          .anyRequest()
+          .permitAll();
     }
 
 }
