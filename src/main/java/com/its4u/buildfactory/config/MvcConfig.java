c2f76c4c3c24b204e594b92f4cb1f8bd9a66eead
'ksc' <'ksc@example.com';>
2022-03-24 16:45:15 +0100

    fix

---
+++ src/main/java/com/its4u/buildfactory/config/MvcConfig.java
@@ -0,0 +1,11 @@
+package com.its4u.buildfactory.config;
+
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class MvcConfig implements WebMvcConfigurer {
+
+	public void addViewControllers(ViewControllerRegistry registry) {
+	
+	}
+}
