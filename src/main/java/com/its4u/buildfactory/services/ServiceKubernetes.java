77880e9df1d37f26305bb24476acfa13ceff78da
'ksc' <'ksc@example.com';>
2022-05-09 17:04:23 +0200

    test

--- src/main/java/com/its4u/buildfactory/services/ServiceKubernetes.java
+++ src/main/java/com/its4u/buildfactory/services/ServiceKubernetes.java
@@ -35,12 +35,13 @@ public class ServiceKubernetes {
 	public void connection(String server,String token) {
 		
 		System.out.println("myToken = "+token);
+		
 		String mytoken="eyJhbGciOiJSUzI1NiIsImtpZCI6IjJ4N3YwTlB1NDFLRGxrQmF2aWNrdTdjcHpId1k5M1Bua3g1RFViMGdPZmsifQ.eyJpc3MiOiJrdWJlcm5ldGVzL3NlcnZpY2VhY2NvdW50Iiwia3ViZXJuZXRlcy5pby9zZXJ2aWNlYWNjb3VudC9uYW1lc3BhY2UiOiJjYXBhY2l0eS10b29sLWRldiIsImt1YmVybmV0ZXMuaW8vc2VydmljZWFjY291bnQvc2VjcmV0Lm5hbWUiOiJjYXBhY2l0eS10b29sLXRva2VuLWM4OGt6Iiwia3ViZXJuZXRlcy5pby9zZXJ2aWNlYWNjb3VudC9zZXJ2aWNlLWFjY291bnQubmFtZSI6ImNhcGFjaXR5LXRvb2wiLCJrdWJlcm5ldGVzLmlvL3NlcnZpY2VhY2NvdW50L3NlcnZpY2UtYWNjb3VudC51aWQiOiJiNWMxOGJiZi02YjAwLTRhZTEtYTE4MC04NjRjYzhkZDM2MmQiLCJzdWIiOiJzeXN0ZW06c2VydmljZWFjY291bnQ6Y2FwYWNpdHktdG9vbC1kZXY6Y2FwYWNpdHktdG9vbCJ9.O4g3Cq3hEtdzR7cxPEuBq3PUsFsLTVKQpYNsHIlOTt3PxclvyS5y-A40GYt1Wemrgbs68MYE_aHhIgJZcDu_ZJQANPpTsjiK0UCidItc-r7-aVGcjyKnNusywpgctuAaI0xgRAApNh1-U0ndeEDuy_2X5ECB3AaKyo6QRR_ovFxmk-IEKtVdiIaN8aaYSgGDhVMV6eE9xSokDU5GlSPvadcwroEbR3uEC90n1lpZ0hcWooZ7RrSHzF6OcGsNh9sQdrXEVkaw3YFTjPymyKnM71XkNj6Ql9KtlfBXWkDvFLrpg5a7VVfIaJdmX_cK3O3C0rLYMOu-zCMMjmjCfiO3IQ";
 		
 		
 		Config config = new ConfigBuilder().withMasterUrl(server)
 				.withDisableHostnameVerification(true)				
-				.withOauthToken(mytoken)
+				.withOauthToken(token)
 				.withTrustCerts(false)
 				.build();
 		
